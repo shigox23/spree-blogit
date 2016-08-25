a0531c4b4f741da828813bd81d9d8f3dd8cfa4b0
Edwin Horneij <edwin@astekwallcovering.com>
2016-08-25 14:45:27 -0700

    Add tag handling back

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
-# SpreeBlogit::Engine.routes.draw do
 Spree::Core::Engine.routes.draw do
+  # Keep these above the posts resources block
+  get 'blog/page/:page' => 'blogit/posts#index'
+  get 'blog/tagged/:tag' => 'blogit/posts#tagged', as: :tagged_blog_posts
 
-  # blog = Spree::Blogit
   namespace :blog, module: :blogit do
-  # # Keep these above the posts resources block
-    # get "page/:page" => "spree/blogit/posts#index"
-    # get "tagged/:tag" => 'posts#tagged', as: :tagged_blog_posts
-    #
-
     resources :posts do
         resources :comments, only: [:create, :destroy]
     end
-
-    get '/' => 'blogit/posts#index', as: :blog_root
+    get '/' => 'posts#index', as: :root
   end
-
 end
