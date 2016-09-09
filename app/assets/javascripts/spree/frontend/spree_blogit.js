1b13bacad85d34716084c94d27bb3a46b166123f
Edwin Horneij <edwin@astekwallcovering.com>
2016-09-09 11:46:35 -0700

    Move archive JS to frontend/spree_blogit.js, remove unused JS files

--- app/assets/javascripts/spree/frontend/spree_blogit.js
+++ app/assets/javascripts/spree/frontend/spree_blogit.js
@@ -1,2 +1,10 @@
 // Placeholder manifest file.
 // the installer will append this file to the app vendored assets here: vendor/assets/javascripts/spree/frontend/all.js'
+
+// Supporting functions for the display of post archives
+jQuery(function(){
+    $("a[data-blogit-click-to-toggle-children]").click(function(event){
+        event.preventDefault();
+        $(this).siblings('ul').toggle();
+    })
+})
