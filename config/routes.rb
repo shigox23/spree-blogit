f4518fb817b6cd7d58a60e06efa5d56c5b6659c2
joshlikesyou <joshlikesyou@gmail.com>
2016-09-23 12:01:11 -0700

    added slugs to related posts links

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Spree::Core::Engine.routes.draw do
   get 'blog/page/:page' => 'blogit/posts#index'
   get 'blog/tagged/:tag' => 'blogit/posts#tagged', as: :tagged_blog_posts
 
+  patch	'/admin/blog/posts/:id/edit' => 'admin/blogit/posts#edit' 
+
   namespace :blog, module: :blogit do
     resources :posts, param: :slug do
         resources :comments, only: [:create, :destroy]
