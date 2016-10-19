cc1e398e0cdf379634316766c350292e511c9c69
Rodrigo Vega <vegarodrigo20@hotmail.com>
2016-10-19 13:02:14 -0400

    Adding Bootsy

--- app/overrides/add_blog_to_spree_nav.rb
+++ app/overrides/add_blog_to_spree_nav.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
     :virtual_path => 'spree/shared/_main_nav_bar',
-    :name => 'add_blog_to_spree_nav',
-    :insert_after => '[data-hook="home-link"]',
+    :name => 'add_blog',
+    :insert_after => '#home-link[data-hook]',
     :text => '<li id="home-link">
-      <%= link_to Spree.t(:blog), spree.blog_path %></li>'
+      <%= link_to Spree.t(:blog), blog_root_path %></li>'
 )
