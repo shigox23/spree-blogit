db74a884f5779a219a4aefc2860c24ac075b2fb7
Rodrigo Vega <vegarodrigo20@hotmail.com>
2016-10-19 15:51:17 -0400

    injecting bootsy js and css to spree store

--- lib/generators/spree_blog/install/install_generator.rb
+++ lib/generators/spree_blog/install/install_generator.rb
@@ -5,13 +5,17 @@ module SpreeBlog
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js',"//= require bootsy", "//= require spree/frontend/spree_blog\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js',"//= require bootsy", "//= require spree/backend/spree_blog\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_blog\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_blog\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require bootsy\n"
+        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require bootsy\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css',"*= require bootsy", " *= require spree/frontend/spree_blog\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css',"*= require bootsy", " *= require spree/backend/spree_blog\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_blog\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_blog\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require bootsy\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require bootsy\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
